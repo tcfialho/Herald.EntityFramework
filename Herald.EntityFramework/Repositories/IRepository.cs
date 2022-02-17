2c7282a049826c82c0808cc9f2cd425300a7d0a2
Thiago F <tcfialho@gmail.com>
2022-02-16 22:48:54 -0300

    feat: making all methods async

--- Herald.EntityFramework/Repositories/IRepository.cs
+++ Herald.EntityFramework/Repositories/IRepository.cs
@@ -5,7 +5,7 @@ namespace Herald.EntityFramework.Repositories
     public interface IRepository<TEntity> : IReadonlyRepository<TEntity> where TEntity : class
     {
         Task Insert(TEntity entity);
-        void Update(TEntity entity);
-        void Delete(TEntity entity);
+        Task Update(TEntity entity);
+        Task Delete(TEntity entity);
     }
 }
